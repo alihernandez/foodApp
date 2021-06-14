be3f983b2853586dd7e45b96c72254a2d2d38590
phantomramen <ali.hernandez9two@gmail.com>
2021-06-13 22:11:17 -0600

    onClick function

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,48 @@ import Data from "./data.json";
 
 function App() {
 
-  let random =() => {
-    console.log("it works!"); 
+  // let randValue = Data[Math.floor(Math.random() * Data.length)];
+  
+
+
+  function myFunction(Data) {
+    let myData = Data;
+    // console.log(myData);
+    let food = myData[Math.floor(Math.random() * myData.length)];
+    console.log(food);
+    
+
+    
+    
+    var x = document.getElementById("re");
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+
+    
     
   }
+
+  
+
+// document.getElementById('select').onblur = function () {
+//     this.style.display = 'none';
+// };
   return (
     <div className="App">
-      <button onClick={random}>click</button>
+      <button className="toggle" onClick={myFunction}>click</button>
 
-      <div className="restaurants">
+      <div id="re" className="restaurants">
         { Data.map(post => {
           return(
 
-            <>
+            <div key={ post.id } className="restaurant">
             <h4>{ post.title }</h4>
             <p>{ post.connent} </p>
-            </>
+            </div>
           )
         }) }
 
